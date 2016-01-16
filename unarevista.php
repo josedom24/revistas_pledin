38ee85b71d52553b5effb089285cfa036e036ee1
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2016-01-16 22:02:58 +0100

    Create unarevista.php

---
+++ unarevista.php
@@ -0,0 +1,25 @@
+[insert_php]
+$base="https://s3-eu-west-1.amazonaws.com/revistas/";
+$d=$_GET["id"];
+$rev=(string)$d;
+if(strlen($rev)==1) $rev="0".$rev;
+$lines = file($base.$rev."/info.txt");
+echo '<h1><a href="http://www.josedomingo.org/pledin/revistas">Revistas Informáticas</a></h1>';
+echo '<h2>'.$lines[0].'</h2>';
+echo '<a href="'.trim($lines[1]).'">Página web</a>';
+array_splice($lines, 0, 2);
+$lines=array_reverse($lines);
+$cont2=1;
+
+echo '<table width="100%" border="0"><tbody>';
+echo '<tr>';
+foreach ($lines as $line_num => $line) { 
+	$dimg=$base.$rev.'/'.(count($lines)-$cont2+1).'.jpg';  
+	$dnum=$base.$rev.'/'.(count($lines)-$cont2+1).'.pdf'; 
+	echo '<td width="25%" valign="top"><a href="'.$dnum.'"><img src="'.$dimg.'" alt="'.$cont.'" width="100px" height="150px" border="0" hspace="0" vspace="0" /></a><br />'.$line.'<br /> <br /></td>';
+	if($cont2%4==0) echo '</tr><tr>'; 
+	$cont2++;
+}
+echo '</tr></tbody></table>';
+
+[/insert_php]
